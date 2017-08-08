192a511a66938bf6d39a2c1c5f000d9e269730a3
Yurii Nosach <nosach.yurko@gmail.com>
2017-08-08 18:17:10 +0300

    Create base model and enums.

---
+++ TMS.TodoApi/Models/AccessToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TMS.TodoApi.Models
+{
+    public class Token
+    {
+        public Token()
+        {
+        }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int SecondsToExpire { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        public DateTime ExpireTime { get; set; }
+
+        public bool IsActive => DateTime.Now < ExpireTime;
+
+        #region System.Object methods
+
+        public override string ToString()
+        {
+            return AccessToken;
+        }
+
+        #endregion
+    }
+}
---
+++ TMS.TodoApi/Models/TodoTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMS.TodoApi.Enums;
+
+namespace TMS.TodoApi.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskPriority Priority { get; set; }
+        public TaskStatus Status { get; set; }
+
+        public string PriorityString { get; set; }
+        public string StatusString { get; set; }
+
+        #region System.Object methods
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is TodoTask task)
+            {
+                return task.Title.Equals(Title, StringComparison.CurrentCultureIgnoreCase);
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
